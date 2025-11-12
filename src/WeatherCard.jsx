6309f2dec8600ceae7cb24849d9b6c0f3b8d305a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-11-12 08:22:13 +0100

    Commit 3

---
+++ src/WeatherCard.jsx
@@ -0,0 +1,13 @@
+// src/WeatherCard.jsx
+export function WeatherCard({ current }) {
+    return (
+      <div className="bg-white rounded-xl p-4 shadow-md w-full max-w-sm">
+        <h2 className="text-xl font-bold">{current.datetime}</h2>
+        <p className="text-lg">{current.conditions}</p>
+        <p>🌡 Temp: {current.temp}°C</p>
+        <p>💨 Viento: {current.windspeed} km/h</p>
+        <p>🌧 Prob. lluvia: {current.precipprob || 0}%</p>
+      </div>
+    );
+  }
+  
